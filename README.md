b8d491a32bd478b7b69e1b9f2b4ee55f9c9ce71e
jarvis-owl <jarvis-owl@googlemail.com>
2018-03-08 21:49:03 +0100

    initial commit & setup repo structure

---
+++ README.md
@@ -0,0 +1,5 @@
+# website-data
+
+``` 08.05.'18  ```
+
+Here araises the website to display my sensordata collected by my RPi.
---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+# @Author: https://github.com/jarvis-owl <jarvis>
+# @Date:   2018-03-08T21:42:22+01:00
+# @Last modified by:   jarvis
+# @Last modified time: 2018-03-08T21:48:47+01:00
+# @License: GPL v3
+
+#show sql with php
+#src: http://www.newthinktank.com/2014/09/php-mysql-tutorial/
+
+
+
+
+?>
